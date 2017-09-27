164e9326896fa3711172a690b7483feedca6212f
j-kayes <31102962+j-kayes@users.noreply.github.com>
2017-09-27 19:57:34 +0100

    Issue with phasers built in collision detection.

---
+++ www/js/circle-object.js
@@ -0,0 +1,20 @@
+class CircleObject {
+    constructor(game, position, radius, colour) {
+        this.game = game;
+        this.radius = radius;
+        this.gameObject = game.add.sprite(position.x, position.y, null);
+
+        game.physics.arcade.enable(this.gameObject);
+        this.gameObject.body.setCircle(radius);
+        this.gameObject.body.bounce = 0.2;
+        this.gameObject.body.collideWorldBounds = true;
+        this.gameObject.circleSprite = this;
+
+        this.colour = colour;
+    }
+
+    draw() {
+        this.game.drawObject.beginFill(this.colour, 1);
+        this.circleImage = this.game.drawObject.drawCircle(this.gameObject.x-this.radius, this.gameObject.y-this.radius, 2*this.radius);
+    }
+}
--- www/js/game.js
+++ www/js/game.js
@@ -1,25 +1,74 @@
-var game = new Phaser.Game(window.innerWidth * window.devicePixelRatio, window.innerHeight * window.devicePixelRatio, Phaser.AUTO, 'game', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(window.innerWidth * window.devicePixelRatio, window.innerHeight * window.devicePixelRatio, Phaser.AUTO, 'game', { preload: preload, create: create, update: update, render: render });
 var socket = io.connect('localhost:3000'); // Pass in the server address. 
-var graphicsObject = game.add.graphics(0, 0);
+game.socket = socket;
+game.pickups = [];
+
+const playerColour = 0xFFFFFF;
+const pickupColours = [0xFF0000, 0x00FF00, 0x0000FF];
+const pickupRadius = 5;
 
 function preload() {
+    game.load.image('background', 'assets/background.png');
     game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    game.drawObject = game.add.graphics(0, 0);
 
     cursors = game.input.keyboard.createCursorKeys();
 }
 function create() {
-    game.world.resize(1000, 1000);
+    game.world.resize(10000, 10000);
+    //game.add.tileSprite(0, 0, 256, 256, 'background'); // Only in top left.
+    game.collectablesGroup = game.add.physicsGroup();
+    generatePickups(200);
 
     var ranX = Math.random() * game.world.width;
     var ranY = Math.random() * game.world.height;
-    player = new CircleObject(game, graphicsObject, 10, {x: 0, y:0}, socket, { x: ranX, y: ranY });
-    spawnOtherPlayers();
-    socket.emit("newPlayer", { id: socket.id, position: player.getPosition() });
+    game.player = new CircleObject(game, { x: ranX, y: ranY }, 25, playerColour);
+
+    //socket.emit("newPlayer", { id: socket.id, position: player.getPosition() });
 }
 function update() {
-    game.camera.x = player.position.x - game.width/2;
-    game.camera.y = player.position.y - game.height/2;
+    game.physics.arcade.moveToPointer(game.player.gameObject, 60, game.input.activePointer, 500);
 
-    player.update(game.time.elapsed);
+    // TODO: Consider writing my own collision detection logic, as this doesn't work as the size of the image is 0.
+    game.physics.arcade.collide(game.player.gameObject, game.collectablesGroup, collectableCollision, null, this);
+    
+    game.camera.x = game.player.gameObject.position.x - game.width/2;
+    game.camera.y = game.player.gameObject.position.y - game.height/2;
 
- }
+    // TODO: user normal for loop for this.
+    game.collectablesGroup.forEach(function(pickup){
+        pickup.circleSprite.draw();
+    }, this);
+
+    game.player.draw();
+}
+function render() {
+    game.drawObject.clear();
+}
+function generatePickups(number) {
+    for(let i = 0; i < number; i++) {
+        let random = Math.random();
+        let ranX = Math.random() * game.world.width;
+        let ranY = Math.random() * game.world.height;
+        if(random < 0.4) {
+            game.collectablesGroup.add(createCircleObject({ x: ranX, y: ranY }, pickupRadius,  pickupColours[0]));
+            //game.pickups.push(new CircleObject(game, { x: ranX, y: ranY },  pickupRadius, pickupColours[0]));
+        }
+        else if(random < 0.8) {
+            game.collectablesGroup.add(createCircleObject({ x: ranX, y: ranY }, pickupRadius,  pickupColours[1]));
+            //game.pickups.push(new CircleObject(game, { x: ranX, y: ranY },  pickupRadius, pickupColours[1]));
+        }
+        else{
+            game.collectablesGroup.add(createCircleObject({ x: ranX, y: ranY }, pickupRadius,  pickupColours[2]));
+            //game.pickups.push(new CircleObject(game, { x: ranX, y: ranY },  5, pickupColours[2]));
+        }
+        
+    }
+}
+function createCircleObject(position, radius, colour) {
+    let circle = new CircleObject(game, position, radius, colour);
+    return circle.gameObject;
+}
+function collectableCollision(object1, object2) {
+    console.log("collision");
+}
