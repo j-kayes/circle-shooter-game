7dcbb5012ca1296e11c6930f5a1783707de37dea
j-kayes <31102962+j-kayes@users.noreply.github.com>
2017-09-28 13:00:04 +0100

    Only draws pickups if they are on the screen.
    Pickups are again only drawn if they are on the screen.

--- www/js/game.js
+++ www/js/game.js
@@ -16,9 +16,8 @@ function preload() {
 }
 function create() {
     game.world.resize(10000, 10000);
-    //game.add.tileSprite(0, 0, 256, 256, 'background'); // Only in top left.
-    game.collectablesGroup = game.add.physicsGroup();
-    generatePickups(200);
+    //game.add.tileSprite(0, 0, 256, 256, 'background'); // Only in top left
+    game.pickups = generatePickups(500);
 
     var ranX = Math.random() * game.world.width;
     var ranY = Math.random() * game.world.height;
@@ -30,15 +29,18 @@ function update() {
     game.physics.arcade.moveToPointer(game.player.gameObject, 60, game.input.activePointer, 500);
 
     // TODO: Consider writing my own collision detection logic, as this doesn't work as the size of the image is 0.
-    game.physics.arcade.collide(game.player.gameObject, game.collectablesGroup, collectableCollision, null, this);
+    //game.physics.arcade.collide(game.player.gameObject, game.collectablesGroup, collectableCollision, null, this);
     
     game.camera.x = game.player.gameObject.position.x - game.width/2;
     game.camera.y = game.player.gameObject.position.y - game.height/2;
 
-    // TODO: user normal for loop for this.
-    game.collectablesGroup.forEach(function(pickup){
-        pickup.circleSprite.draw();
-    }, this);
+    for(var i = 0; i < game.pickups.length; i++) {
+        if(game.pickups[i].gameObject.x < (game.camera.x + game.width) && game.pickups[i].gameObject.x > game.camera.x) {
+            if(game.pickups[i].gameObject.y < (game.camera.y + game.height) && game.pickups[i].gameObject.y > game.camera.y) {
+                game.pickups[i].draw();
+            }
+        }
+    }
 
     game.player.draw();
 }
@@ -46,24 +48,23 @@ function render() {
     game.drawObject.clear();
 }
 function generatePickups(number) {
+    let pickups = [];
     for(let i = 0; i < number; i++) {
         let random = Math.random();
         let ranX = Math.random() * game.world.width;
         let ranY = Math.random() * game.world.height;
         if(random < 0.4) {
-            game.collectablesGroup.add(createCircleObject({ x: ranX, y: ranY }, pickupRadius,  pickupColours[0]));
-            //game.pickups.push(new CircleObject(game, { x: ranX, y: ranY },  pickupRadius, pickupColours[0]));
+            pickups.push(new CircleObject(game, { x: ranX, y: ranY },  pickupRadius, pickupColours[0]));
         }
         else if(random < 0.8) {
-            game.collectablesGroup.add(createCircleObject({ x: ranX, y: ranY }, pickupRadius,  pickupColours[1]));
-            //game.pickups.push(new CircleObject(game, { x: ranX, y: ranY },  pickupRadius, pickupColours[1]));
+            pickups.push(new CircleObject(game, { x: ranX, y: ranY },  pickupRadius, pickupColours[1]));
         }
         else{
-            game.collectablesGroup.add(createCircleObject({ x: ranX, y: ranY }, pickupRadius,  pickupColours[2]));
-            //game.pickups.push(new CircleObject(game, { x: ranX, y: ranY },  5, pickupColours[2]));
+            pickups.push(new CircleObject(game, { x: ranX, y: ranY },  pickupRadius, pickupColours[2]));
         }
         
     }
+    return pickups;
 }
 function createCircleObject(position, radius, colour) {
     let circle = new CircleObject(game, position, radius, colour);
